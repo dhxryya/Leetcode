c3190557216f0e525469d519a4d3d91260ebdfc5
Dhairya Tiwari <tdhairya507@gmail.com>
2026-06-24 20:53:23 +0530

    Time: 0 ms (100%), Space: 23.4 MB (44.05%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,13 +3,5 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        i = 0
-        j = len(s)-1
-
-        while i<j:
-           temp =s[i]
-           s[i] = s[j]
-           s[j] = temp
-
-           i+=1
-           j-=1
+        #isse ek line me ho jyega kyuki reverse string lga di hai
+        s.reverse() 
